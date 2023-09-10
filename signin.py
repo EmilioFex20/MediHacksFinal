03b3770d96a5d5cd0585f0afc57bfe27e807ea17
Xobi22 <derek.beltran@cetys.edu.mx>
2023-09-09 19:13:34 -0700

    Update signin.py

--- signin.py
+++ signin.py
@@ -1,8 +1,2 @@
-from tkinter import *
-from PIL import ImageTk
-import mysql.connector
-root=Tk()
-
-bgImage=ImageTk.PhotoImage(file='background.jpg')
-
-root.mainloop()
+from kivy.uix.screenmanager import ScreenManager
+from kivymd.app import MDApp
